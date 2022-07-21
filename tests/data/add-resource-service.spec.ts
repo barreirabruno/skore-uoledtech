bd9227d9cb5c4f0c4e229aa027276dd2ed9499c5
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-21 00:38:39 -0300

    test: ensure it calls Content Resource Repository with correct params

--- tests/data/add-resource-service.spec.ts
+++ tests/data/add-resource-service.spec.ts
@@ -2,6 +2,7 @@ import ContentResourceRepo from '@/infra/database/class-persistence-db'
 import AddContentResourceService from '@/data/add-content-resource-service'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+import { ContentResource } from '@/domain/entities'
 
 describe('Add Content Resource service', () => {
   let contentResourceRepository: MockProxy<ContentResourceRepo>
@@ -19,6 +20,7 @@ describe('Add Content Resource service', () => {
   })
 
   beforeEach(() => {
+    jest.clearAllMocks()
     addContentResourceService = new AddContentResourceService(contentResourceRepository)
   })
 
@@ -32,5 +34,21 @@ describe('Add Content Resource service', () => {
       type: 'pdf',
       createdAt: expect.any(Date)
     })
+  })
+
+  it('should call Content Resource Repository with correct params', async () => {
+    const newContentResource = new ContentResource({
+      id: 'any_content_resource_input_id',
+      name: 'any_content_resource_input_name',
+      published: 1,
+      description: 'any_content_resource_input_description',
+      type: 'pdf',
+      createdAt: new Date(Date.now())
+    })
+    const contentResourceSpy = jest.spyOn(contentResourceRepository, 'add')
+    await addContentResourceService.perform(inputParams)
+    expect(contentResourceSpy).toHaveBeenCalled()
+    expect(contentResourceSpy).toHaveBeenCalledTimes(1)
+    expect(contentResourceSpy).toHaveBeenCalledWith(newContentResource)
   })
 })
