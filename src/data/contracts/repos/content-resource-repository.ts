803aa0ec1c902ed073dc9ba24a660ed008a9bbd9
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-22 01:51:03 -0300

    test: add LoadContentResourceRepositoryInterface

--- src/data/contracts/repos/content-resource-repository.ts
+++ src/data/contracts/repos/content-resource-repository.ts
@@ -1,5 +1,23 @@
 import { InternalServerError } from '@/domain/entities/errors/internal-server-error'
 
+export interface LoadContentResourceRepositoryInterface {
+  load: (input: LoadTransactionRepositoryNamespace.Input) => Promise<LoadTransactionRepositoryNamespace.Output>
+}
+
+export namespace LoadTransactionRepositoryNamespace {
+  export type Input = {
+    id: string
+  }
+  export type Output = {
+    id: string
+    published: number
+    name: string
+    description: string
+    type: string
+    createdAt: Date
+  } | InternalServerError | undefined
+}
+
 export interface SaveContentResourceRepositoryInterface {
   save: (input: SaveTransactionRepositoryNamespace.Input) => Promise<SaveTransactionRepositoryNamespace.Output>
 }
