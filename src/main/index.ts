05c1595083105e4f2df31e09cff2176da7f2a561
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-22 16:19:25 -0300

    refactor: add try/catch for error threatment when start server

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,16 @@
 import './config/module-alias'
 
+import 'reflect-metadata'
+import { createConnection } from 'typeorm'
+
 void (async () => {
-  const PORT = 3333
-  const { setupApp } = await import('./config/app')
-  const app = await setupApp()
-  app.listen(PORT, () => console.log(`[SERVER UP AND RUNNING ON][http://localhost:${PORT}]`))
+  try {
+    const PORT = 3333
+    const { setupApp } = await import('./config/app')
+    const app = await setupApp()
+    await createConnection()
+    app.listen(PORT, () => console.log(`[SERVER UP AND RUNNING ON][http://localhost:${PORT}]`))
+  } catch (error) {
+    console.log('[ERROR]', error)
+  }
 })()
