3124d093351d0050d6b35396c1170ab6ec05591d
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-22 12:43:41 -0300

    refactor: use PgContentResourceRepository on factory

--- src/main/factories/add-content-resource-controller.ts
+++ src/main/factories/add-content-resource-controller.ts
@@ -1,9 +1,9 @@
 import AddContentResourceService from '@/data/add-content-resource-service'
-import ContentResourceRepo from '@/infra/database/class-persistence-db'
+import PgContentResourceRepository from '@/infra/database/postgres/repos/content-resource-repo'
 import { AddContentResourceController } from '@/presentation/controllers'
 
 export const makeAddContentResourceController = (): AddContentResourceController => {
-  const persistenceClass = new ContentResourceRepo()
+  const persistenceClass = new PgContentResourceRepository()
   const addContentResourceService = new AddContentResourceService(persistenceClass)
   return new AddContentResourceController(addContentResourceService)
 }
