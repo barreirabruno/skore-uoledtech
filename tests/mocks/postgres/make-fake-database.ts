508c0bc9bc8a75661188aa4915ec32ee74e9fdeb
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-22 10:14:16 -0300

    refactor: move makeFakeDb to a single file

---
+++ tests/mocks/postgres/make-fake-database.ts
@@ -0,0 +1,11 @@
+import { IMemoryDb, newDb } from 'pg-mem'
+
+export const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
+  const db = newDb()
+  const connection = await db.adapters.createTypeormConnection({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/pg-content-resource.ts']
+  })
+  await connection.synchronize()
+  return db
+}
