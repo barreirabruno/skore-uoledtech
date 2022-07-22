da7a0c9b0a177e72e95289eebf7664726fe8934d
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-22 12:43:17 -0300

    refactor: remove ContentResourceRepo

--- src/infra/database/class-persistence-db.ts
+++
@@ -1,9 +0,0 @@
-import { ContentResource } from '@/domain/entities'
-
-export default class ContentResourceRepo {
-  contentResourcesList: ContentResource[] = []
-
-  add (newContentResource: ContentResource): void {
-    this.contentResourcesList.push(newContentResource)
-  }
-}
