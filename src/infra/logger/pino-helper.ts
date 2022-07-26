5d069cd47a25606e63eee81b2bb4c1227c94420b
brunobarreira <brunobarreirasilva@gmail.com>
2022-07-26 18:58:39 -0300

    chore: add logger

--- src/infra/logger/pino-helper.ts
+++ src/infra/logger/pino-helper.ts
@@ -1,8 +1,11 @@
 import pino, { Logger } from 'pino'
+import env from '@/main/config/environment-variables'
 
 export const pinoHelper = {
   createLogger (): Logger {
     return pino({
+      enabled: env.logger.enable as unknown as boolean | undefined,
+      level: env.logger.level,
       transport: {
         target: 'pino-pretty',
         options: {
@@ -11,7 +14,12 @@ export const pinoHelper = {
       }
     })
   },
-  logInfo (stack: string): void {
-    this.createLogger().info({ stackinfo: stack })
+  logInfo (object: any, layer: string, stack: string): void {
+    const trace = JSON.stringify({ object, layer, stackinfo: stack })
+    this.createLogger().info(trace)
+  },
+  logError (error: any): void {
+    const trace = JSON.stringify(error)
+    this.createLogger().info(trace)
   }
 }
