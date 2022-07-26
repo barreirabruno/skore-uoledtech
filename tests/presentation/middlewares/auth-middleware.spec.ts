f51a55dc3ce536d4576e6e5affb66e6ad71cdf38
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-25 23:54:51 -0300

    refactor: remove implementation classes from test file

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,30 +1,20 @@
 import { AccessDeniedError } from '@/domain/entities/errors/access-denied-error'
-import { forbidenError, HttpResponse, serverError, success } from '@/presentation/helpers'
-import { Middleware } from '@/presentation/helpers/middleware'
-
-class AuthorizationMiddleware implements Middleware {
-  constructor (
-    private readonly role: string
-  ) {}
-
-  async handle (httpRequest: any): Promise<HttpResponse<any>> {
-    try {
-      if (httpRequest.role !== this.role) return forbidenError(new AccessDeniedError())
-      return success({ id: 'any_static_id' })
-    } catch (error) {
-      return serverError(error as Error)
-    }
-  }
-}
+import AuthorizationMiddleware from '@/presentation/middlewares/authorization-middleware'
 
 describe('Auth middleware', () => {
+  let sut: AuthorizationMiddleware
+
+  beforeEach(() => {
+    sut = new AuthorizationMiddleware('ADMIN')
+  })
+
   it('should return forbiden if roles are different', async () => {
-    const sut = await new AuthorizationMiddleware('ADMIN').handle({ role: 'STUDENT' })
-    expect(sut).toEqual({ statusCode: 403, data: new AccessDeniedError() })
+    const checkRole = await sut.handle({ role: 'STUDENT' })
+    expect(checkRole).toEqual({ statusCode: 403, data: new AccessDeniedError() })
   })
 
   it('should return id if ADMIN role is allowed', async () => {
-    const sut = await new AuthorizationMiddleware('ADMIN').handle({ role: 'ADMIN' })
-    expect(sut).toEqual({ statusCode: 200, data: { id: 'any_static_id' } })
+    const checkRole = await sut.handle({ role: 'ADMIN' })
+    expect(checkRole).toEqual({ statusCode: 200, data: { id: 'any_static_id' } })
   })
 })
