6e95d1f8ba6a3325467952a6167f86182197059e
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 00:46:09 -0300

    feat: use directive on apollo-server implementation

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import typeDefs from '../graphql/type-defs'
 import resolvers from '../graphql/resolvers'
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
 import { ApolloServer } from 'apollo-server-express'
 import { GraphQLError } from 'graphql'
+import { authorizationDirectiveTransformer } from '../graphql/directives'
 
 const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
   errors?.forEach(error => {
@@ -24,9 +26,11 @@ const checkError = (error: GraphQLError, errorName: string): boolean => {
   return [error.name, error.originalError?.name].some(name => name === errorName)
 }
 
+let schema = makeExecutableSchema({ resolvers, typeDefs })
+schema = authorizationDirectiveTransformer('ADMIN', schema)
+
 export const setupApolloServer = (): ApolloServer => new ApolloServer({
-  resolvers,
-  typeDefs,
+  schema,
   context: ({ req }) => ({ req }),
   plugins: [{
     requestDidStart: async () => ({
