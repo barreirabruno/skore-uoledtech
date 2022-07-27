77c7ea1e60a243e026bbb3bac2f3ae3fb23ac67e
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 23:04:00 -0300

    test: ensure it returns RequiredFieldError if value is undefined

--- tests/__unit/presentation/validation/required-string-validator.spec.ts
+++ tests/__unit/presentation/validation/required-string-validator.spec.ts
@@ -1,6 +1,15 @@
+import { RequiredFieldError } from '@/presentation/errors/required-field-error'
 import RequiredStringValidator from '@/presentation/validation/required-string-validator'
 
 describe('Required string validator', () => {
+  it('should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
   it('should return empty if value is not empty, null or undefined', () => {
     const sut = new RequiredStringValidator('any_valid_tring', 'any_field')
 
