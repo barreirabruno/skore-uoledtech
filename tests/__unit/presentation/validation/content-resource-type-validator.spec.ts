0449f87848a603683608dcf9668042cf7e94a4b0
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 23:20:39 -0300

    test: ensure it returns undefined when content type resource is PDF | IMAGE | VIDEO

--- tests/__unit/presentation/validation/content-resource-type-validator.spec.ts
+++ tests/__unit/presentation/validation/content-resource-type-validator.spec.ts
@@ -16,6 +16,17 @@ export default class ContentResourceValidator {
 }
 
 describe('Content resource type validator', () => {
+  const validContentResourceTypes = ['pdf', 'image', 'video']
+  validContentResourceTypes.forEach(validContentResource => {
+    it(`should return undefined when content type resource if ${validContentResource}`, () => {
+      const sut = new ContentResourceValidator(`${validContentResource}`)
+
+      const error = sut.validate()
+
+      expect(error).toBeUndefined()
+    })
+  })
+
   it('should return ContentResourceValidator if value is empty', () => {
     const sut = new ContentResourceValidator('not_valid_content_type')
 
