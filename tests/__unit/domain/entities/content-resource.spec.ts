370517c9826ebf4844a130c570d35513d6b90279
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 15:58:49 -0300

    test: ensure it creates a Content Resource with loadParam

--- tests/__unit/domain/entities/content-resource.spec.ts
+++ tests/__unit/domain/entities/content-resource.spec.ts
@@ -1,26 +1,43 @@
 import { ContentResource } from '@/domain/entities'
 
 const inputParams = {
-  id: 'any_content_resource_input_id',
   name: 'any_content_resource_input_name',
   published: 1,
   description: 'any_content_resource_input_description',
+  type: 'pdf'
+}
+
+const loadDataParams = {
+  id: '1',
+  name: 'update_content_name',
+  published: 1,
+  description: 'update_content_description',
   type: 'pdf',
   created_at: new Date(Date.now()).toISOString(),
   updated_at: new Date(Date.now()).toISOString()
 }
 
 describe('Content Resource entity', () => {
-  it('should create a Content Resource with correct params', () => {
-    const sut = new ContentResource(inputParams)
+  it('should create a Content Resource with loadParams', () => {
+    const sut = new ContentResource(inputParams, loadDataParams)
     expect(sut).toEqual({
-      id: 'any_content_resource_input_id',
-      name: 'any_content_resource_input_name',
+      id: '1',
+      name: 'update_content_name',
       published: 1,
-      description: 'any_content_resource_input_description',
+      description: 'update_content_description',
       type: 'pdf',
       created_at: expect.any(String),
       updated_at: expect.any(String)
+    })
+  })
+
+  it('should create a Content Resource with correct params', () => {
+    const sut = new ContentResource(inputParams)
+    expect(sut).toEqual({
+      name: 'any_content_resource_input_name',
+      published: 1,
+      description: 'any_content_resource_input_description',
+      type: 'pdf'
     })
   })
 })
