65e976dc96b5421db8b14059e02c4a1732ef5a16
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 23:06:28 -0300

    test: ensure it returns RequiredFieldError if value is empty

--- tests/__unit/presentation/validation/required-string-validator.spec.ts
+++ tests/__unit/presentation/validation/required-string-validator.spec.ts
@@ -2,6 +2,14 @@ import { RequiredFieldError } from '@/presentation/errors/required-field-error'
 import RequiredStringValidator from '@/presentation/validation/required-string-validator'
 
 describe('Required string validator', () => {
+  it('should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
   it('should return RequiredFieldError if value is null', () => {
     const sut = new RequiredStringValidator(null as any, 'any_field')
 
