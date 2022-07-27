08d8f5e6cb98b5e7c8443327f8b33f95e55a36f2
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 16:40:52 -0300

    refactor: pass a Content Resource instance to database repository

--- src/data/add-content-resource-service.ts
+++ src/data/add-content-resource-service.ts
@@ -1,4 +1,4 @@
-import { ContentResource } from '@/domain/entities'
+// import { ContentResource } from '@/domain/entities'
 import { AddContentResourceInterface, AddContentResourceNamespace } from '@/domain/features/add-resource-feature'
 import PgContentResourceRepository from '@/infra/database/postgres/repos/content-resource-repo'
 import { pinoHelper } from '@/infra/logger/pino-helper'
@@ -10,13 +10,7 @@ export default class AddContentResourceService implements AddContentResourceInte
 
   async perform (params: AddContentResourceNamespace.Input): Promise<AddContentResourceNamespace.Output> {
     pinoHelper.logInfo(params, 'data', 'Raw object that will be sent to repository')
-    const newContentResource = new ContentResource({
-      published: params.published,
-      name: params.name,
-      description: params.description,
-      type: params.type
-    })
-    const add = await this.contentResourceRepo.save(newContentResource)
+    const add = await this.contentResourceRepo.save(params)
     return add
   }
 }
