be44339e7209cca33d923a642a7ae3a41fdbe8af
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 22:58:19 -0300

    test: ensure it returns 400 if validation fails

--- tests/__unit/presentation/controllers/add-content-resource-controller.spec.ts
+++ tests/__unit/presentation/controllers/add-content-resource-controller.spec.ts
@@ -1,7 +1,11 @@
 import AddContentResourceService from '@/data/add-content-resource-service'
 import { AddContentResourceController } from '@/presentation/controllers'
+import ValidationComposite from '@/presentation/validation/validation-composite'
+import { mocked } from 'jest-mock'
 
 import { mock, MockProxy } from 'jest-mock-extended'
+
+jest.mock('@/presentation/validation/validation-composite')
 
 describe('Add Content Resource Controller', () => {
   let contentResourceRepository: MockProxy<AddContentResourceService>
@@ -26,6 +30,29 @@ describe('Add Content Resource Controller', () => {
 
   afterEach(() => {
     jest.clearAllMocks()
+  })
+
+  it('should return 400 if validation fails', async () => {
+    const error = new Error('Validation Error')
+    const validationCompositeSpy = jest.fn().mockImplementationOnce(() => ({
+      validate: jest.fn().mockReturnValueOnce(error)
+    }))
+    mocked(ValidationComposite).mockImplementationOnce(validationCompositeSpy)
+
+    const httpResponse = await sut.handle({
+      params: {
+        name: '',
+        published: 1,
+        description: 'any_content_resource_input_description',
+        type: 'pdf'
+      }
+    })
+
+    expect(ValidationComposite).toHaveBeenCalledWith([])
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: error
+    })
   })
 
   it('should call AddContentResourceService with correct params', async () => {
