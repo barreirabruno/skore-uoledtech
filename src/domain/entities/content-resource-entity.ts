0ca4d0a093dd22a11756c701f721ed50d39bcaca
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 15:57:15 -0300

    refactor: use optional data to create a Content Resource entity

--- src/domain/entities/content-resource-entity.ts
+++ src/domain/entities/content-resource-entity.ts
@@ -8,6 +8,16 @@ type ContentResourceData = {
   updated_at?: string
 }
 
+type LoadContentResourceData = {
+  id: string
+  published: number
+  name: string
+  description: string
+  type: string
+  created_at: string
+  updated_at: string
+}
+
 export class ContentResource {
   id?: string
   published: number
@@ -17,13 +27,13 @@ export class ContentResource {
   created_at?: string
   updated_at?: string
 
-  constructor (params: ContentResourceData) {
-    this.id = params.id
-    this.published = params.published
-    this.name = params.name
-    this.description = params.description
-    this.type = params.type
-    this.created_at = params.created_at
-    this.updated_at = params.updated_at
+  constructor (params: ContentResourceData, loadParams?: LoadContentResourceData) {
+    this.id = loadParams?.id ?? params.id
+    this.published = loadParams?.published ?? params.published
+    this.name = loadParams?.name ?? params.name
+    this.description = loadParams?.description ?? params.description
+    this.type = loadParams?.type ?? params.type
+    this.created_at = loadParams?.created_at ?? params.created_at
+    this.updated_at = loadParams?.updated_at ?? params.updated_at
   }
 }
