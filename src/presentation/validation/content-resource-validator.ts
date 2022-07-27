443fea97a24b438ee73c086f6bdd10040bfc20b7
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 00:12:25 -0300

    refactor: use value to validate content resource valid types

--- src/presentation/validation/content-resource-validator.ts
+++ src/presentation/validation/content-resource-validator.ts
@@ -2,12 +2,13 @@ import { InvalidContentTypeError } from '../errors/invalid-content-type-error'
 
 export default class ContentResourceValidator {
   constructor (
+    private readonly value: string,
     private readonly fieldName: string
   ) {}
 
   validate (): Error | undefined {
     const contentTypeAvailable = ['pdf', 'image', 'video']
-    const validateType = contentTypeAvailable.indexOf(this.fieldName)
+    const validateType = contentTypeAvailable.indexOf(this.value)
     if (validateType === -1) {
       return new InvalidContentTypeError(this.fieldName)
     }
