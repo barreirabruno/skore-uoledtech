63353fbd8483396dfca53a856f677d809562711c
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 19:38:33 -0300

    refactor: ensure it creates a Content Resource with loadParams

--- tests/__unit/domain/entities/content-resource.spec.ts
+++ tests/__unit/domain/entities/content-resource.spec.ts
@@ -9,23 +9,26 @@ const inputParams = {
 
 const loadDataParams = {
   id: '1',
-  name: 'update_content_name',
+  name: 'database_any_name_value',
   published: 1,
-  description: 'update_content_description',
-  type: 'pdf',
+  description: 'database_any_description_value',
+  type: 'image',
   created_at: new Date(Date.now()).toISOString(),
   updated_at: new Date(Date.now()).toISOString()
 }
 
 describe('Content Resource entity', () => {
   it('should create a Content Resource with loadParams', () => {
+    const inputParams = {
+      name: 'update_any_name_value'
+    }
     const sut = new ContentResource(inputParams, loadDataParams)
     expect(sut).toEqual({
       id: '1',
-      name: 'update_content_name',
+      name: 'update_any_name_value',
       published: 1,
-      description: 'update_content_description',
-      type: 'pdf',
+      description: 'database_any_description_value',
+      type: 'image',
       created_at: expect.any(String),
       updated_at: expect.any(String)
     })
