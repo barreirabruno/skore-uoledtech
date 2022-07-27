a4845ef593c405ab171836d42b79cedc6e954352
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-26 22:57:29 -0300

    refactor: use validation on base controller

--- src/presentation/controllers/controller-base.ts
+++ src/presentation/controllers/controller-base.ts
@@ -1,5 +1,6 @@
 import { pinoHelper } from '@/infra/logger/pino-helper'
-import { HttpResponse, serverError } from '../helpers'
+import { badRequest, HttpResponse, serverError } from '../helpers'
+import ValidationComposite from '../validation/validation-composite'
 import { Validator } from '../validation/validator'
 
 export abstract class Controller {
@@ -10,11 +11,20 @@ export abstract class Controller {
   }
 
   async handle (httpRequest: any): Promise<HttpResponse> {
+    const error = this.validate(httpRequest)
+    if (error !== undefined) {
+      return badRequest(error)
+    }
     try {
       return await this.perform(httpRequest)
     } catch (error) {
       pinoHelper.logError(error, 'Error on controller execution')
       return serverError(error as Error)
     }
+  }
+
+  private validate (httpRequest: any): Error | undefined {
+    const validators = this.buildValidators(httpRequest)
+    return new ValidationComposite(validators).validate()
   }
 }
