91412c5f3ecf6649776d17580f7e9466afebb8a4
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 00:36:36 -0300

    feat: add PublishedValidator

---
+++ src/presentation/validation/published-validator.ts
@@ -0,0 +1,15 @@
+import { InvalidFieldError } from '../errors/invalid-field-error'
+
+export default class PublishedValidator {
+  constructor (
+    private readonly value: string,
+    private readonly fieldName: string
+  ) {}
+
+  validate (): Error | undefined {
+    if (parseInt(this.value) !== 1) {
+      return new InvalidFieldError(this.fieldName)
+    }
+    return undefined
+  }
+}
