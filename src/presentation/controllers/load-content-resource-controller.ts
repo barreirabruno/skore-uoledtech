6d5d95bc61169449a74054a5523970c83ed47040
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-28 01:43:47 -0300

    refactor: return null if content resource is not found by id

--- src/presentation/controllers/load-content-resource-controller.ts
+++ src/presentation/controllers/load-content-resource-controller.ts
@@ -1,5 +1,5 @@
 import LoadContentResourceService from '@/data/load-content-resource-service'
-import { HttpResponse, success } from '../helpers'
+import { HttpResponse, noContent, success } from '../helpers'
 import { Controller } from './controller-base'
 
 export default class LoadContentResourceController extends Controller {
@@ -10,7 +10,10 @@ export default class LoadContentResourceController extends Controller {
   }
 
   async perform (httpRequest: any): Promise<HttpResponse<any>> {
-    const loadContentResource = await this.loadContentResourceService.perform(httpRequest)
+    const loadContentResource = await this.loadContentResourceService.perform(httpRequest.params)
+    if (loadContentResource === null) {
+      return noContent(null)
+    }
     return success(loadContentResource)
   }
 }
