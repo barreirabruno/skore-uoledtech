08984afc74ea58fa8d02add3187c5144fa5ca377
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-28 01:44:24 -0300

    refactor: return undefined if published attribute is not 1

--- src/data/load-content-resource-service.ts
+++ src/data/load-content-resource-service.ts
@@ -8,6 +8,9 @@ export default class LoadContentResourceService implements LoadContentResourceIn
 
   async perform (params: LoadContentResourceNamespace.Input): Promise<LoadContentResourceNamespace.Output> {
     const loadContentResource = await this.contentResourceRepo.load({ id: params.id })
+    if (loadContentResource?.published !== 1) {
+      return undefined
+    }
     return loadContentResource
   }
 }
