63d2468a08a8f3f6d174988e6b50c3fbd0744dc1
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 22:49:19 -0300

    test: ensure it deactivates a content resource found on database

--- tests/__unit/data/deactivate-resource-service.spec.ts
+++ tests/__unit/data/deactivate-resource-service.spec.ts
@@ -16,6 +16,24 @@ describe('Deactivate content resource', () => {
     sut = new DeactivateContentResourceService(contentResourceRepository)
   })
 
+  it('should deactivate a content resource found on database', async () => {
+    contentResourceRepository.load.mockResolvedValue({
+      id: 'any_id_from_database',
+      name: 'any_name_value_from_database',
+      published: 1,
+      description: 'any_description_value_from_database',
+      type: 'pdf',
+      created_at: new Date(Date.now()).toISOString(),
+      updated_at: new Date(Date.now()).toISOString()
+    })
+    contentResourceRepository.deactivate.mockResolvedValueOnce()
+    const deactivateContentResource = await sut.perform({ id: 'any_valid_content_resource_id' })
+    expect(deactivateContentResource).toEqual({
+      id: 'any_valid_content_resource_id',
+      message: 'Content deactivated successfully'
+    })
+  })
+
   it('should change published value from 1 to 0', async () => {
     contentResourceRepository.load.mockResolvedValue(null)
     contentResourceRepository.deactivate.mockResolvedValue()
