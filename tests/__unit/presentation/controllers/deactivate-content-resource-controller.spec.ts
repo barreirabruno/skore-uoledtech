9769f03a4fd262b7970ed5e5fa9683897fe36d60
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-27 22:37:45 -0300

    test: ensure it returns 204 if perform method succeeds

--- tests/__unit/presentation/controllers/deactivate-content-resource-controller.spec.ts
+++ tests/__unit/presentation/controllers/deactivate-content-resource-controller.spec.ts
@@ -7,6 +7,8 @@ describe('Deactivate content resource', () => {
   let deactivateContentResourceService: MockProxy<DeactivateContentResourceService>
   let sut: DeactivateContentResourceController
 
+  const inputParams = { params: { id: 'any_valid_id' } }
+
   beforeAll(() => {
     deactivateContentResourceService = mock()
   })
@@ -25,10 +27,39 @@ describe('Deactivate content resource', () => {
       id: 'any_valid_id',
       message: 'Content resource could not be deactivated'
     })
-    const input = { params: { id: 'any_valid_id' } }
-    await sut.perform(input)
+
+    await sut.perform(inputParams)
     expect(spyService).toHaveBeenCalled()
     expect(spyService).toHaveBeenCalledTimes(1)
     expect(spyService).toHaveBeenCalledWith({ id: 'any_valid_id' })
+  })
+
+  it('should call DeactivateContentResourceService with correct params', async () => {
+    const spyService = jest.spyOn(deactivateContentResourceService, 'perform')
+    deactivateContentResourceService.perform.mockResolvedValue({
+      id: 'any_valid_id',
+      message: 'Content resource could not be deactivated'
+    })
+
+    await sut.perform(inputParams)
+    expect(spyService).toHaveBeenCalled()
+    expect(spyService).toHaveBeenCalledTimes(1)
+    expect(spyService).toHaveBeenCalledWith({ id: 'any_valid_id' })
+  })
+
+  it('should return 204 if perform method succeeds', async () => {
+    deactivateContentResourceService.perform.mockResolvedValueOnce({
+      id: 'any_valid_id',
+      message: 'Content deactivated successfully'
+    })
+    const httpResponse = await sut.perform(inputParams)
+
+    expect(httpResponse).toEqual({
+      statusCode: 204,
+      data: {
+        id: 'any_valid_id',
+        message: 'Content deactivated successfully'
+      }
+    })
   })
 })
