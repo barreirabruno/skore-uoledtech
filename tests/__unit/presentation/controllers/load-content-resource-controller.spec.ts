5594e8417bedef6df3c9be5af60ef5e2572178d0
barreirabruno <brunobarreirasilva@gmail.com>
2022-07-28 01:02:17 -0300

    test: ensure it calls LoadContentResourceService with correct params

--- tests/__unit/presentation/controllers/load-content-resource-controller.spec.ts
+++ tests/__unit/presentation/controllers/load-content-resource-controller.spec.ts
@@ -20,15 +20,6 @@ describe('Load content resource controller', () => {
   let loadContentResourceService: MockProxy<LoadContentResourceService>
   let sut: LoadContentResourceController
 
-  const inputParams = {
-    params: {
-      name: 'any_content_resource_input_name',
-      published: 1,
-      description: 'any_content_resource_input_description',
-      type: 'pdf'
-    }
-  }
-
   beforeAll(() => {
     loadContentResourceService = mock()
   })
@@ -41,6 +32,24 @@ describe('Load content resource controller', () => {
     jest.clearAllMocks()
   })
 
+  it('should call LoadContentResourceService with correct params', async () => {
+    const spyService = jest.spyOn(loadContentResourceService, 'perform')
+    loadContentResourceService.perform.mockResolvedValue({
+      id: 'any_content_resource_input_id',
+      name: 'any_content_resource_input_name',
+      published: 1,
+      description: 'any_content_resource_input_description',
+      type: 'pdf',
+      created_at: expect.any(String),
+      updated_at: expect.any(String)
+    })
+
+    await sut.perform({ id: 'any_valid_id' })
+    expect(spyService).toHaveBeenCalled()
+    expect(spyService).toHaveBeenCalledTimes(1)
+    expect(spyService).toHaveBeenCalledWith({ id: 'any_valid_id' })
+  })
+
   it('should return 200 if perform method succeeds', async () => {
     loadContentResourceService.perform.mockResolvedValueOnce({
       id: 'any_content_resource_input_id',
@@ -51,7 +60,7 @@ describe('Load content resource controller', () => {
       created_at: expect.any(String),
       updated_at: expect.any(String)
     })
-    const httpResponse = await sut.perform(inputParams)
+    const httpResponse = await sut.perform({ id: 'any_valid_id' })
 
     expect(httpResponse).toEqual({
       statusCode: 200,
